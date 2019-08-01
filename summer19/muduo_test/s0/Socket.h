f764c84c8bbf15e42f2c5af114e1497443af7486
lgjh123 <leegaojin@xiyoulinux.org>
2019-08-01 16:05:48 +0800

    add sockets*

---
+++ summer19/muduo_test/s0/Socket.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <boost/noncopyable.hpp>
+
+class InetAddress;
+
+class Socket : boost::noncopyable
+{
+public:
+    explicit Socket(int sockfd)
+        :sockfd_(sockfd)
+    {  }
+
+    ~Socket();
+    
+    int fd() const { return sockfd_; }
+    //如果address被使用出abord
+    void bindAddress(const InetAddress& localaddr);
+
+    void listen();
+
+    int accept(InetAddress* peeraddr);
+
+    //Enable/disable SO_REUSEADDR ??
+    void setReuseAddr(bool on);
+
+private:
+
+    const int sockfd_;
+};
+
